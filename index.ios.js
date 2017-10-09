cfe0ac3f93992bc7999de23d8b0ac72f62b9b50f
Masayuki Iwai <miyabi@mybdesign.com>
2017-10-09 13:36:30 +0900

    Support status bar visibility change.

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,7 @@
  */
 'use strict'
 
-import { NativeModules, NativeEventEmitter, Platform } from 'react-native'
+import { NativeModules, NativeEventEmitter } from 'react-native'
 import type EmitterSubscription from 'EmitterSubscription'
 
 export type SafeAreaInsets = { top: number, left: number, bottom: number, right: number };
@@ -13,9 +13,7 @@ const nativeEventEmitter = new NativeEventEmitter(nativeModule)
 
 class SafeArea {
   getSafeAreaInsetsForRootView(): Promise<{ safeAreaInsets: SafeAreaInsets }> {
-    return parseInt(Platform.Version, 10) >= 11
-      ? nativeModule.getSafeAreaInsetsForRootView()
-      : { top: 20, left: 0, bottom: 0, right: 0 }
+    return nativeModule.getSafeAreaInsetsForRootView()
   }
 
   addEventListener(eventType: string, listener: Function, context: ?Object): ?EmitterSubscription {
--- ios/RNSafeArea/RNSafeArea.m
+++ ios/RNSafeArea/RNSafeArea.m
@@ -11,62 +11,82 @@
 
 @implementation RNSafeArea
 
+BOOL statusBarHiddenInitially = NO;
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getSafeAreaInsetsForRootView:(RCTPromiseResolveBlock)resolve
                   rejector:(RCTPromiseRejectBlock)reject) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIEdgeInsets safeAreaInsets = UIEdgeInsetsMake(0, 0, 0, 0);
-        UIWindow *window = RCTSharedApplication().keyWindow;
-        if (window) {
-            UIViewController *rootViewController = window.rootViewController;
-            if (rootViewController && rootViewController.view) {
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
-                if (@available(iOS 11.0, *)) {
-                    safeAreaInsets = rootViewController.view.safeAreaInsets;
-                }
-#endif
-            }
-        }
-        
+        UIEdgeInsets safeAreaInsets = [self safeAreaInsetsForRootView];
         resolve([self constructResult:safeAreaInsets]);
     });
 }
 
-#pragma mark - RCTEventEmitter implementation
+#pragma mark -
 
-- (NSArray<NSString *> *)supportedEvents {
-    return @[@"safeAreaInsetsForRootViewDidChange"];
+- (id)init {
+    if ((self = [super init])) {
+        statusBarHiddenInitially = [[[NSBundle mainBundle] objectForInfoDictionaryKey:@"UIStatusBarHidden"] boolValue];
+    }
+    return self;
 }
 
-- (void)startObserving {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(safeAreaInsetsForRootViewDidChange:)
-                                                 name:RNSafeAreaInsetsForRootViewDidChangeNotification
-                                               object:nil];
-}
-
-- (void)stopObserving {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (void)safeAreaInsetsForRootViewDidChange:(NSNotification *)notification {
-    RCTRootView *rootView = (RCTRootView *)notification.object;
-    UIEdgeInsets safeAreaInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+- (CGFloat)statusBarHeight {
+    UIApplication *sharedApplication = RCTSharedApplication();
     
-    if (rootView) {
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
-        if (@available(iOS 11.0, *)) {
-            safeAreaInsets = rootView.safeAreaInsets;
-        }
-#endif
+    if (statusBarHiddenInitially && sharedApplication.isStatusBarHidden) {
+        return 0.0f;
     }
     
-    [self sendEventWithName:@"safeAreaInsetsForRootViewDidChange"
-                       body:[self constructResult:safeAreaInsets]];
+    UIWindow *window = sharedApplication.keyWindow;
+    if (window) {
+        UIViewController *rootViewController = window.rootViewController;
+        if (rootViewController) {
+            if (rootViewController.prefersStatusBarHidden) {
+                return 0.0f;
+            }
+        }
+    }
+    
+    // TODO: It would be better to use sharedApplication.statusBarFrame.size.height,
+    // but it has unexpected value at this time.
+    return 20.0f;
 }
 
-#pragma mark -
+- (UIEdgeInsets)safeAreaInsetsForView:(UIView *)view {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+        if (view) {
+            return view.safeAreaInsets;
+        }
+    }
+#endif
+    
+    UIEdgeInsets safeAreaInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    
+    if (view) {
+        if ([view isKindOfClass:[RCTRootView class]]) {
+            safeAreaInsets.top = [self statusBarHeight];
+        }
+    }
+    
+    return safeAreaInsets;
+}
+
+- (UIEdgeInsets)safeAreaInsetsForRootView {
+    UIView *rootView = nil;
+    
+    UIWindow *window = RCTSharedApplication().keyWindow;
+    if (window) {
+        UIViewController *rootViewController = window.rootViewController;
+        if (rootViewController) {
+            rootView = rootViewController.view;
+        }
+    }
+    
+    return [self safeAreaInsetsForView:rootView];
+}
 
 - (NSDictionary *)constructResult:(UIEdgeInsets)safeAreaInsets {
     return @{
@@ -77,6 +97,49 @@ RCT_EXPORT_METHOD(getSafeAreaInsetsForRootView:(RCTPromiseResolveBlock)resolve
                      @"right": @(safeAreaInsets.right),
                      },
              };
+}
+
+- (void)notifySafeAreaInsetsForRootViewDidChange:(UIEdgeInsets)safeAreaInsets {
+    [self sendEventWithName:@"safeAreaInsetsForRootViewDidChange" body:[self constructResult:safeAreaInsets]];
+}
+
+#pragma mark - RCTEventEmitter implementation
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"safeAreaInsetsForRootViewDidChange"];
+}
+
+- (void)startObserving {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
+    if (@available(iOS 11.0, *)) {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(safeAreaInsetsForRootViewDidChange:)
+                                                     name:RNSafeAreaInsetsForRootViewDidChangeNotification
+                                                   object:nil];
+        return;
+    }
+#endif
+    
+    // Fallback for older version of iOS
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidChangeStatusBarFrame:)
+                                                 name:UIApplicationDidChangeStatusBarFrameNotification
+                                               object:nil];
+}
+
+- (void)stopObserving {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)safeAreaInsetsForRootViewDidChange:(NSNotification *)notification {
+    RCTRootView *rootView = (RCTRootView *)notification.object;
+    UIEdgeInsets safeAreaInsets = [self safeAreaInsetsForView:rootView];
+    [self notifySafeAreaInsetsForRootViewDidChange:safeAreaInsets];
+}
+
+- (void)applicationDidChangeStatusBarFrame:(NSNotification *)notification {
+    UIEdgeInsets safeAreaInsets = [self safeAreaInsetsForRootView];
+    [self notifySafeAreaInsetsForRootViewDidChange:safeAreaInsets];
 }
 
 @end
